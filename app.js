ef3846207d0bf97e9e59b239f1feb117fd98e6f3
nuzzesick <mati280341@gmail.com>
2021-04-05 02:39:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('colors');
+const { showMenu, pause } = require('./helpers/messages');
+
+console.clear();
+
+const main = async () => {
+  let opt = '';
+  do {
+    opt = await showMenu();
+    console.log({ opt });
+    if (opt !== '0') await pause();
+  } while (opt !== '0');
+}
+
+main();
